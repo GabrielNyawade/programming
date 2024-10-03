b4fefaaade80782cfa6b6f57dc1c09628e7457d6
GabrielNyawade <gabrielopiyo1133@gmail.com>
2024-10-04 00:34:19 +0300

    Student Files

--- C_/matrix.c
+++ C_/matrix.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int a, b, c, d, e, f, g, h, i;
-
+void terminate_exe(const char* error_message)
+{
+    printf("%s\n", error_message);
+    exit(1);
+}
 
 void createMatrix();
+void printMatrix();
 
 int main()
 {
+    int option;
     printf("MATRIX CALCULATOR\n");
     printf("1. Create a matrix\n");
     printf("2. Edit a matrix\n");
     printf("3. Get determinant of a matrix\n");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+    case 1:
+        createMatrix();
+        break;
+    
+    default:
+        break;
+    }
     return 0;
 }
 
@@ -22,12 +39,44 @@ void createMatrix()
     scanf("%d", &m);
     printf("n = ?\n");
     scanf("%d", &n);
-    int matrix[m][n];
+    if (m <= 0 && n <= 0)
+    terminate_exe("Invalid dimensions (Accepts positive non-zero integers)\n");
 
-    if (m == 1 && n == 1)
+
+
+    int **matrix = (int **)malloc(m * sizeof(int *));
+    if (matrix == NULL) 
+    terminate_exe("Memory allocation (m) failed!\n");
+
+    for (int iter = 0; iter < m; iter++)
     {
-        printf("Mat 1,1?");
-        scanf("%d", &a);
-        matrix = {a};
+        matrix[m] = (int *)malloc(n * sizeof(int));
+        if (matrix[m] == NULL)
+        terminate_exe("Memory allocation (n) failed!");
     }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("Mat %d,%d = ?\n", i+1, j+1);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix created!");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    for (int i = 0; i < m; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
 }
+
