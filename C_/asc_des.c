ad9614a6fc1f8a78ce5cedb4d22401cad7cf10e9
Gabriel Opiyo <gabrielopiyo1133@gmail.com>
2025-03-29 13:19:48 +0300

    Make additions

---
+++ C_/asc_des.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+    printf("Enter number:\n");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; i++)
+        printf("%d", i);
+
+    printf("\n");
+    for (i = num; i >= 1; i--)
+        printf("%d", i);
+    return 0;
+}
--- object_oriented/linked_list.cpp
+++ object_oriented/linked_list.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
+// Definition of a Node in a singly linked list
 struct Node
 {
     int data;
-    int *ptr;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = nullptr;
+    }
 };
 
 Node *head = NULL;
